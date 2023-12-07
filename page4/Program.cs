362f1f9893a2e1e5d1a914d808f897dc6ee19c7d
yy <410570138@m365.fju.edu.tw>
2023-12-07 13:08:47 +0800

    1207!!!!

--- page4/Program.cs
+++ page4/Program.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Components.Web;
+﻿using System;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using page4;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -10,3 +14,92 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.
 
 await builder.Build().RunAsync();
 
+public partial class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.RootComponents.Add<App>("app");
+
+        // 設定應用程式的配置
+        builder.Configuration.AddJsonFile("appsettings.json");
+        builder.Configuration.AddJsonFile("appsettings.Development.json", true);
+
+        // 在這裡加入你的 DbContext 配置
+        ConfigureDbContext(builder);
+
+        // 設定 DbContext
+        builder.Services.AddDbContext<RecordDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("RecordDbContext")));
+
+        // 配置 HTTP Client
+        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+        await builder.Build().RunAsync();
+    }
+
+    private static void ConfigureDbContext(WebAssemblyHostBuilder builder)
+    {
+        var connection = String.Empty;
+
+        if (builder.HostEnvironment.IsDevelopment())
+        {
+            builder.Configuration.AddJsonFile("appsettings.Development.json");
+            connection = builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING");
+        }
+        else
+        {
+            connection = Environment.GetEnvironmentVariable("AZURE_SQL_CONNECTIONSTRING");
+        }
+        builder.Services.AddDbContext<RecordDbContext>(options =>
+            options.UseSqlServer(connection));
+
+    }
+}
+
+
+
+    /*
+     * private static void Configure(WebAssemblyHostBuilder builder)
+    {
+        // 在這裡新增以下端點
+        builder.Services.AddScoped<RecordDbContext>();
+
+        builder.Services.AddScoped(sp =>
+        {
+            var context = sp.GetRequiredService<RecordDbContext>();
+            return context.Record.ToList();
+        })
+        .WithName("GetRecords")
+        .WithOpenApi();
+
+        builder.Services.AddScoped(sp =>
+        {
+            var context = sp.GetRequiredService<RecordDbContext>();
+            var record = new Record(); // 根據你的實際需求初始化 Person
+            context.Add(Record);
+            context.SaveChanges();
+        })
+        .WithName("CreateRecord")
+        .WithOpenApi();
+    }
+
+}
+
+
+
+        app.MapGet("/Record", (RecordDbContext context) =>
+        {
+            return context.Person.ToList();
+        })
+        .WithName("GetRecords")
+        .WithOpenApi();
+
+        app.MapPost("/Record", (Record Record, RecordDbContext context) =>
+        {
+            context.Add(Record);
+            context.SaveChanges();
+        })
+        .WithName("CreateRecord")
+        .WithOpenApi();
+*/
---
+++ page4/RecordDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace page4
+{ 
+    public class RecordDbContext : DbContext
+    {
+        public RecordDbContext(DbContextOptions<RecordDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Record> Records { get; set; }
+    }
+
+    public class Record
+    {
+        public string id_card { get; set; }
+        public string patient_id { get; set; }
+        public string patient_name { get; set; }
+        public string gender { get; set; }
+        public DateTime birth_date { get; set; }
+        public string phone_number { get; set; }
+    }
+}
